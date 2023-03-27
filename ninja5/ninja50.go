de4200580f55e69b44a681ce1e05c9621c274d30
Ricardo <cadym1997@gmail.com>
2023-03-27 00:30:44 -0300

    Inicio dos exercícios ninja nvl 5

---
+++ ninja5/ninja50.go
@@ -0,0 +1,41 @@
+package main
+//Crie um tipo "pessoa" com tipo subjacente struct, que possa conter os seguintes campos:
+// - Nome, - Sobrenome, - Sabores favoritos de sorvete
+//Crie dois valores do tipo "pessoa" e demonstre estes valores, utilizando range na slice
+// que contem os sabores de sorvete.
+
+import "fmt"
+
+type pessoa struct {
+	nome 			string 
+	sobrenome 		string
+	sabores			[]string
+}
+
+func main() {
+
+	pessoa1 := pessoa{
+		nome: "João",
+		sobrenome: "Silva",
+		sabores: []string{"napolitano", "creme"},
+	}
+	
+	// Usando uma forma reduzida para preencher os dados da struct
+	pessoa2 := pessoa{
+		"José",
+		"Pereira",
+		[]string{"chocolate", "mroango"},
+	}
+
+	fmt.Println("Meu nome é", pessoa1.nome, pessoa1.sobrenome, "e meus sabores favoritos de sorvete são:")
+	for _, v := range pessoa1.sabores{
+		fmt.Println("-", v)
+	}
+
+	fmt.Printf("\n")
+
+	fmt.Println("Meu nome é", pessoa2.nome, pessoa2.sobrenome, "e meus sabores favoritos de sorvete são:")
+	for _, v := range pessoa2.sabores{
+		fmt.Println("-", v)
+	}
+}
